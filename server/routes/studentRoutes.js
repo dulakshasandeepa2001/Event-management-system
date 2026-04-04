e4a2ca2b3798cd9990fafe45250221161c1cd7c6
Dulaksha Sandeepa <93658848+dulakshasandeepa2001@users.noreply.github.com>
2026-04-04 07:21:40 +0530

    Fix duplicate imports in studentRoutes.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,9 +1,5 @@
 import express from "express";
-<<<<<<< HEAD
-import { getAllStudents, getStudentById, setStudentManualInactive } from "../controllers/studentController.js";
-=======
 import { getAllStudents, getStudentById, getStudentsByBatch, setStudentManualInactive } from "../controllers/studentController.js";
->>>>>>> ra_new_part
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -12,10 +8,9 @@ const router = express.Router();
 router.use(protect);
 
 router.get("/", getAllStudents);
-<<<<<<< HEAD
-=======
+
 router.get("/batch/:batchId", getStudentsByBatch);
->>>>>>> ra_new_part
+
 router.get("/:id", getStudentById);
 
 router.patch("/:id/manual-deactivate", protect, setStudentManualInactive);
